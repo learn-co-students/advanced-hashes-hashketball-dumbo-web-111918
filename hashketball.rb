d3dcb3ed265758a092535441bdc4ce8abafb13f8
Roman Pirag <mr.anadale@yahoo.com>
2018-11-10 23:09:11 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -116,129 +116,66 @@ def game_hash
 end
 
 def num_points_scored(player)
-  game_hash[:home][:players].map do |name, stats| 
-    if name == player 
-      return stats[:points]
-    end
-  end
-  game_hash[:away][:players].map do |name, stats| 
-    if name == player 
-      return stats[:points]
+  game_hash.each do |team, info|
+    info[:players].each do |name, stat|
+      if name == player 
+        return stat[:points]
+      end
     end
   end
 end
 
 def shoe_size(player)
-  game_hash[:home][:players].map do |name, stats| 
-    if name == player 
-      return stats[:shoe]
-    end
-  end
-  game_hash[:away][:players].map do |name, stats| 
-    if name == player 
-      return stats[:shoe]
-    end
+  game_hash.each do |team, info|
+    info[:players].each do |name, stats|
+      if name == player 
+        return stats[:shoe]
+      end
+    end   
   end
 end
 
 def team_colors(team)
-  if game_hash[:home][:team_name] == team 
-    return game_hash[:home][:colors]
-  end
-  if game_hash[:away][:team_name] == team 
-    return game_hash[:away][:colors]
+  game_hash.each do |local, info| 
+    if info[:team_name] == team 
+      return info[:colors] 
+    end
   end
 end
 
 def team_names
-  all_team_names = []
-  all_team_names << game_hash[:home][:team_name]
-  all_team_names << game_hash[:away][:team_name]
-  return all_team_names
+  game_hash.map do |local, info|
+    info[:team_name]
+  end
 end
 
 def player_numbers(team)
-all_stats = []   
-  if game_hash[:home][:team_name] == team 
-      game_hash[:home][:players].map do |players, info|
-        info.map do |catagory, stats| 
-        if catagory == :number 
-          all_stats << stats
-        end
+  game_hash.each do |local, info|
+    if info[:team_name] == team 
+      return info[:players].map do |name, stat|
+        stat[:number]
       end
     end
   end
-  if game_hash[:away][:team_name] == team 
-      game_hash[:away][:players].map do |players, info|
-        info.map do |catagory, stats| 
-        if catagory == :number 
-          all_stats << stats
-        end
-      end
-    end
-  end
-  return all_stats 
 end
 
 def player_stats(player)
-  player_stats = {}
-  game_hash[:home][:players].map do |name, stats|
-    if name == player 
-      player_stats = stats 
+  game_hash.each do |local, info|
+    if info[:players][player]
+      return info[:players][player]
     end
   end
-  game_hash[:away][:players].map do |name, stats|
-    if name == player 
-      player_stats = stats 
-    end
-  end
-  player_stats
 end
 
-def big_shoe_rebounds
-  
-    # This is obviosly the wrong way to do this. 
-    # But I think I deserve an award for dumbest successful method. 
-    
-    # First I created variables for all the shoe sizes. 
-
-    alan = game_hash[:home][:players]["Alan Anderson"][:shoe]
-    reggie = game_hash[:home][:players]["Reggie Evans"][:shoe]
-    brook = game_hash[:home][:players]["Brook Lopez"][:shoe]
-    mason = game_hash[:home][:players]["Mason Plumlee"][:shoe]
-    jason = game_hash[:home][:players]["Jason Terry"][:shoe]
-    jeff = game_hash[:away][:players]["Jeff Adrien"][:shoe]
-    bismak = game_hash[:away][:players]["Bismak Biyombo"][:shoe]
-    desagna = game_hash[:away][:players]["DeSagna Diop"][:shoe]
-    ben = game_hash[:away][:players]["Ben Gordon"][:shoe]
-    brendan = game_hash[:away][:players]["Brendan Haywood"][:shoe]
-    
-    # Then I put the shoe sizes into a hash with the name as a key. 
-
-    largest_shoes = {"Alan Anderson": alan, "Reggie Evans": reggie, "Brook Lopez": brook, "Mason Plumlee": mason, "Jason Terry": jason, "Jeff Adrien": jeff, "Bismak Biyombo": bismak, "Desagna Diop": desagna, "Ben Gordon": ben, "Brendan Haywood": brendan }
-
-    # Then I iterated through largest_shoes and set the highest 
-    # shoe size as largest_shoe and the shoe_name to the respective name.
-    # Then once I had the name of the largest shoe player in "shoe_name",
-    # I check if the name matches the players in :away and :home. 
-    # If it finds it, it returns the rebounds of that player. 
-
-largest_shoe = 0
-shoe_name = nil
-    largest_shoes.map do |name, shoe| 
-      if shoe > largest_shoe
-        largest_shoe = shoe
-        shoe_name = name
-      end
+def big_shoe_rebounds 
+  players_shoes = {}
+  game_hash.each do |local, info| 
+    info[:players].each do |name, stats|
+      players_shoes[stats[:shoe]] = name 
     end
-    game_hash[:away][:players].each do |name, info|
-      if name == shoe_name.to_s
-        return game_hash[:away][:players][shoe_name.to_s][:rebounds]
-      end
+    largest_shoes = players_shoes.sort[-1][1]
+    if info[:players][largest_shoes]
+      return info[:players][largest_shoes][:rebounds]
     end
-    game_hash[:home][:players].each do |name, info|
-      if name == shoe_name.to_s
-        return game_hash[:home][:players][shoe_name.to_s][:rebounds]
-      end
-    end
+  end
 end
